2ac753466096003e944f7a5925e1baf120aca2dd
Alexander Williams <williams_alex@protonmail.com>
2019-09-26 22:24:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return "cat"
+  animal = "cat"
+  return animal
 }
 
 function add2(n) {
-  return n + two
+  return n + 2
 
   // Feel free to move things around!
   const two = 2
